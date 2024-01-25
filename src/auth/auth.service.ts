412f5138f97c560ec99cefda38bdfffbe6f62e81
“CaesarBourne” <caesaradek@gmail.com>
2024-01-25 08:45:02 +0100

    auth service changes'

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,70 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  private logger = new Logger('AuthService');
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+    // if (user && user.password === pass) {
+    //   const { password, ...result } = user;
+    //   return result;
+    // }
+    console.log('username $$$ ', username);
+
+    this.logger.debug(
+      ` Value checkd in database if user exists   "${JSON.stringify(user)}" `,
+    );
+
+    if (user) {
+      return { status: 1, message: 'user already exists' };
+    } else {
+      return { status: 0, message: 'New User' };
+    }
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async login(user: any) {
+    const existingUser = await this.validateUser(user.username);
+    this.logger.verbose(
+      `User status in database    "${JSON.stringify(existingUser)}" `,
+    );
+    if (existingUser.status == 0) {
+      throw new UnauthorizedException(
+        'User does not exist in database, please provide Valid user ',
+      );
+    }
+
+    const payload = { username: user.username, sub: user.password };
+    const loginCredentials = {
+      access_token: this.jwtService.sign(payload),
+    };
+
+    this.logger.debug(
+      `Succesfully generated token on server   "${JSON.stringify(
+        loginCredentials,
+      )}" `,
+    );
+
+    return loginCredentials;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+  async register(createUserDto: CreateUserDto) {
+    const existingUser = await this.validateUser(createUserDto.username);
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+    this.logger.debug(
+      `existing user statu   "${JSON.stringify(existingUser)}" `,
+    );
+    if (existingUser.status == 0) {
+      return this.usersService.create(createUserDto);
+    } else {
+      return existingUser;
+    }
   }
 }
